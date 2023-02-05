7eea8ca8dcfdccbb27950f84c4f907830bc9a136
Alexander Bogdanov <alexander@bogdanov.ru>
2023-02-05 20:07:53 +0300

    Added a bean to the application

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,9 +2,21 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class DemoApplication
+class DemoApplication {
+	@Bean
+	fun createBean(): MyBean {
+		println("Bean Created")
+		println()
+		return MyBean()
+	}
+}
+
+class MyBean {
+	
+}
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
